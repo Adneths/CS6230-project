bff179bf273dbccb030f34fd4a49e67678481454
MylesMylesLi <ml2743@cornell.edu>
2023-11-30 16:27:15 -0500

    malloc_ind_data_spec_in_main

--- src/spmspv_test.cpp
+++ src/spmspv_test.cpp
@@ -112,12 +112,12 @@ int main(int argc, char **argv) {
         return 1;
     }
     CSCMatrix<double>* csc_matrix = new CSCMatrix<double>((struct csc_matrix_t*)csc_spm->repr);
-    printf("In main:\n");
-    printf("1.csc_matrix->cols: %d\n", csc_matrix->cols);
-    printf("csc_matrix addr:%d\n", csc_matrix);
+    // printf("In main:\n");
+    // printf("1.csc_matrix->cols: %d\n", csc_matrix->cols);
+    // printf("csc_matrix addr:%d\n", csc_matrix);
 
     struct csr_matrix_t* csr_mat = csc_to_csr((struct csc_matrix_t*)spm->repr);
-    printf("2.csc_matrix->cols: %d\n", csc_matrix->cols);
+    // printf("2.csc_matrix->cols: %d\n", csc_matrix->cols);
 
     CSRMatrix<double>* matrix = new CSRMatrix<double>(csr_mat);
 
@@ -130,17 +130,17 @@ int main(int argc, char **argv) {
     double* data_spvec = (double*)malloc(csc_matrix->cols * sizeof(double));
     sp_rand = cuda::gen_rand_spvec(csc_matrix->cols, sparsity, ind_spvec, data_spvec);
     // sp_rand = SpVector(len, sparsity);
-    printf("3.csc_matrix->cols: %d\n", csc_matrix->cols);
+    // printf("3.csc_matrix->cols: %d\n", csc_matrix->cols);
     lfsp_rand = new LF_SpVector<double>(*sp_rand);
-    printf("4.csc_matrix->cols: %d\n", csc_matrix->cols);
+    // printf("4.csc_matrix->cols: %d\n", csc_matrix->cols);
 
     sp_matdriven = cuda::spmspv_naive_matdriven(matrix, sp_rand);
 
-    printf("In main:\n");
-    printf("csc_smp->repr->m: %d\n", ((struct csc_matrix_t*)(csc_spm->repr))->m);
-    printf("5.csc_matrix->cols: %d\n", csc_matrix->cols);
-    printf("spm != csc_spm: %d\n", static_cast<int>(spm!=csc_spm));
-    printf("csc_matrix addr:%d\n", csc_matrix);
+    // printf("In main:\n");
+    // printf("csc_smp->repr->m: %d\n", ((struct csc_matrix_t*)(csc_spm->repr))->m);
+    // printf("5.csc_matrix->cols: %d\n", csc_matrix->cols);
+    // printf("spm != csc_spm: %d\n", static_cast<int>(spm!=csc_spm));
+    // printf("csc_matrix addr:%d\n", csc_matrix);
     lfsp_vecdriven = cuda::spmspv_naive_vecdriven(csc_matrix, lfsp_rand);
     // result_cusparse = cusparse::spgemm(matrix, matrix);
     //std::cout << matrix << std::endl;
@@ -158,7 +158,8 @@ int main(int argc, char **argv) {
     //     printf("Cuda Result does not match CuSparse Result\n");
 
 
-    // delete matrix;
+    delete matrix;
+    delete csc_matrix;
     delete sp_matdriven;
     delete lfsp_vecdriven;
     // delete sp_rand;
