941c6f86df906765353a5d02dbff8d6cb5e2923a
MylesMylesLi <ml2743@cornell.edu>
2023-11-30 12:07:31 -0500

    dim_unmatch

--- src/SpMSpV_cuda.cu
+++ src/SpMSpV_cuda.cu
@@ -157,6 +157,8 @@ __global__ void spmspv_naive_vecdriven_dacc(int rowsA, int colsA, int* colPtrA, 
 LF_SpVector<double>* spmspv_naive_vecdriven(CSCMatrix<double>* A, LF_SpVector<double>* B){
     if (A->cols != B->len) {
         printf("#Cols of the Matrix dosen't match Len of the Vector!\n");
+        printf("A->cols %d",A->cols);
+        printf("B->len %d", B->len);
         return nullptr;
     }
 #ifdef PROFILE
