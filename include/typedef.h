a9ab94392cb9058cfe3473ea02e8bca495224513
yang kaixin <ky427@cornell.edu>
2023-12-09 17:48:20 -0500

    fix

--- include/typedef.h
+++ include/typedef.h
@@ -305,8 +305,9 @@ struct GCOO
     int *cols;
     int *gIdexs;
     int *nnzpergroup;
-    GCOO(CSRMatrix<T> *mat, int p) // p it the number of rows of a group in sparse matrix
-        : num_row(3)
+    GCOO(struct CSRMatrix<T> *mat, int p) // p it the number of rows of a group in sparse matrix
+        : num_row(mat->rows), num_col(mat->cols), nnz(mat->nnz),
+          num_group((mat->rows - 1 + p) / p), rowPtr(mat->rowPtr), cols(mat->dataCol), values(mat->dataVal)
     {
         printf("hhhhhh");
         int row_index = 0;
