cf68566f3893b27a7c42a697cda5c1c2dbc3cfb8
MylesMylesLi <ml2743@cornell.edu>
2023-12-09 11:24:36 -0500

    new data

--- test_spmspv.sh
+++ test_spmspv.sh
@@ -14,4 +14,5 @@ srun ./spmspv_profile data/micromass_10NN/micromass_10NN.rb            > results
 srun ./spmspv_profile data/umistfacesnorm_10NN/umistfacesnorm_10NN.rb  > results/umistfacesnorm_10NN.txt
 srun ./spmspv_profile data/ex36/ex36.rb                                > results/ex36.txt
 srun ./spmspv_profile data/lock1074/lock1074.rb                        > results/lock1074.txt
-srun ./spmspv_profile data/ia2010/ia2010.rb                            > results/ia2010.txt
+srun ./spmspv_profile data/nv1/nv1.rb                                  > results/nv1.txt
+# srun ./spmspv_profile data/ia2010/ia2010.rb                            > results/ia2010.txt
